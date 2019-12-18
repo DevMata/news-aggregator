a25d0a6d6b19bd6c157d129ed0b09f40e6f5db1c
Antonio Mata <hildeamp@gmail.com>
2019-12-18 13:46:06 -0600

    :recycle: Add type for method get Guardian news

--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -4,33 +4,37 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AxiosResponse } from 'axios';
 
+import { SerializeNewYorkNew, SerializeGuardianNew } from './helpers/newyorktimes/serialize.helper';
+import { New } from './interfaces/New.interface';
+
 const NYT_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?';
-const NYT_FILTERS = '&sort=newest&fl=_id,pub_date,snippet,web_url,headline';
+const NYT_FILTERS = '&sort=relevance&fl=_id,pub_date,snippet,web_url,headline,byline';
 const GUARDIAN_URL = 'https://content.guardianapis.com/search';
+const GUARDIAN_FILTERS = '&show-tags=contributor';
 
 @Injectable()
 export class NewsService {
   constructor(private readonly httpService: HttpService, private readonly configService: ConfigService) {}
 
-  nytSearch(searchTerm: string): Observable<AxiosResponse<object[]>> {
+  newYorkTimeSearch(searchTerm: string): Observable<AxiosResponse<New[]>> {
     try {
       const NYT_KEY = this.configService.get<string>('NYT_KEY');
 
       return this.httpService
         .get(`${NYT_URL}q=${searchTerm}&api-key=${NYT_KEY}${NYT_FILTERS}`)
-        .pipe(map(res => res.data.response.docs));
+        .pipe(map(res => res.data.response.docs.map(SerializeNewYorkNew)));
     } catch (error) {
       console.error(error);
     }
   }
 
-  guardianSearch(searchTerm: string): Observable<AxiosResponse<object[]>> {
+  guardianSearch(searchTerm: string): Observable<AxiosResponse<New[]>> {
     try {
       const GUARDIAN_KEY = this.configService.get<string>('GUARDIAN_KEY');
 
       return this.httpService
-        .get(`${GUARDIAN_URL}?api-key=${GUARDIAN_KEY}&q=${searchTerm}`)
-        .pipe(map(res => res.data.response.results));
+        .get(`${GUARDIAN_URL}?api-key=${GUARDIAN_KEY}&q=${searchTerm}${GUARDIAN_FILTERS}`)
+        .pipe(map(res => res.data.response.results.map(SerializeGuardianNew)));
     } catch (error) {
       console.error(error);
     }
