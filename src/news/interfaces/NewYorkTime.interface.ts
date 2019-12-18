073756e66901e63367f53e19bcd991f7c6809145
Antonio Mata <hildeamp@gmail.com>
2019-12-18 12:02:10 -0600

    :sparkles: Add NewYorkTime New interface

---
+++ src/news/interfaces/NewYorkTime.interface.ts
@@ -0,0 +1,12 @@
+export interface NewYorkTimeNew {
+  web_url: string;
+  snippet: string;
+  headline: {
+    main: string;
+  };
+  byline: {
+    original: string;
+  };
+  pub_date: Date;
+  _id: string;
+}
