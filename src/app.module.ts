9de9e418d589a93654e49c3bec84156cf6ab8556
Antonio Mata <hildeamp@gmail.com>
2019-12-19 14:49:44 -0600

    :recycle: Refactor app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,10 @@ import { ValidateSearchMiddleware } from './middleware/validate-search.middlewar
 import { ValidateSourceParamMiddleware } from './middleware/validate-source-param.middleware';
 import { ConfigModule } from '@nestjs/config';
 import { NewsModule } from './news/news.module';
-import { LoginController } from './login/login.controller';
-import { LoginService } from './login/login.service';
 import { LoginModule } from './login/login.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), NewsModule, LoginModule],
-  controllers: [LoginController],
-  providers: [LoginService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
