7638981f808f95b4ea680282df93acfe2b1dff85
Antonio Mata <hildeamp@gmail.com>
2019-12-19 18:40:49 -0600

    Apply validate json format middleware to login endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { ValidateSearchMiddleware } from './news/middleware/validate-search.middleware';
 import { ValidateSourceParamMiddleware } from './news/middleware/validate-source-param.middleware';
+import { ValidateJsonMiddleware } from './login/middleware/validate-json.middleware';
 import { ConfigModule } from '@nestjs/config';
 import { NewsModule } from './news/news.module';
 import { LoginModule } from './login/login.module';
@@ -11,5 +12,6 @@ import { LoginModule } from './login/login.module';
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
     consumer.apply(ValidateSearchMiddleware, ValidateSourceParamMiddleware).forRoutes('news');
+    consumer.apply(ValidateJsonMiddleware).forRoutes('login');
   }
 }
